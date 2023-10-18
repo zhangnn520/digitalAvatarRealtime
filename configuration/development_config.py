96ba16a9ae2217488d4e794ece62b7f10c4a7367
90houlaoheshang <907333918@qq.com>
2023-10-18 10:23:31 +0800

    改端口

--- configuration/development_config.py
+++ configuration/development_config.py
@@ -5,7 +5,7 @@ from utilities.singleton import singleton
 @singleton
 class Settings(BaseSettings):
     host: str = '0.0.0.0'
-    port: int = 6006
+    port: int = 80
 
     fps: float = 25  # 帧率
     mouth_region_size: int = 256  # help to resize window
